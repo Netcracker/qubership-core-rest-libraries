bbdcbb957677863e3c9244fc7cda259e58c9cffb
Evgenii Stetsenko <i@estetsenko.com>
2025-10-16 16:39:38 +0300

    chore: increase test coverage

--- route-registration/route-registration-common/src/main/java/com/netcracker/cloud/routesregistration/common/gateway/route/v3/domain/RouteConfig.java
+++ route-registration/route-registration-common/src/main/java/com/netcracker/cloud/routesregistration/common/gateway/route/v3/domain/RouteConfig.java
@@ -2,6 +2,7 @@ package com.netcracker.cloud.routesregistration.common.gateway.route.v3.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 import java.util.Objects;
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 @Data
 @Builder
+@EqualsAndHashCode
 public class RouteConfig {
     private String version;
     private List<RouteV3> routes;
@@ -30,25 +32,5 @@ public class RouteConfig {
         });
 
         return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RouteConfig that = (RouteConfig) o;
-        if (!Objects.equals(version, that.version)) {
-            return false;
-        }
-        if (routes == that.routes) {
-            return true;
-        }
-        return routes != null && that.routes != null && routes.size() == that.routes.size()
-                && routes.containsAll(that.routes) && that.routes.containsAll(routes);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(version);
     }
 }
--- route-registration/route-registration-common/src/main/java/com/netcracker/cloud/routesregistration/common/gateway/route/v3/domain/RouteV3.java
+++ route-registration/route-registration-common/src/main/java/com/netcracker/cloud/routesregistration/common/gateway/route/v3/domain/RouteV3.java
@@ -2,12 +2,15 @@ package com.netcracker.cloud.routesregistration.common.gateway.route.v3.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Data
 @Builder
+@EqualsAndHashCode
 public class RouteV3 {
     private RouteDestination destination;
     private List<Rule> rules;
@@ -21,27 +24,14 @@ public class RouteV3 {
     }
 
     private void mergeRules(List<Rule> rulesToMerge) {
-        rulesToMerge.forEach(ruleToMerge -> {
-            if (rules.stream().noneMatch(ruleToMerge::equals)) {
-                rules.add(ruleToMerge);
-            }
-        });
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RouteV3 routeV3 = (RouteV3) o;
-        if (!Objects.equals(destination, routeV3.destination)) {
-            return false;
+        if (rulesToMerge == null || rulesToMerge.isEmpty()) {
+            return;
         }
-        return rules != null && routeV3.rules != null && rules.size() == routeV3.rules.size()
-                && rules.containsAll(routeV3.rules) && routeV3.rules.containsAll(rules);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(destination);
+        List<Rule> currentRules = (rules == null) ? List.of() : rules;
+
+        this.rules = Stream.concat(currentRules.stream(), rulesToMerge.stream())
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
